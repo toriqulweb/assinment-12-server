e4c4637d7270bf818bbc745893aeb02a5746dbd8
toriqulweb <toriqulsenhati@gmail.com>
2025-02-12 23:47:46 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,192 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+
+// const jwt = require('jsonwebtoken');
+// const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+    ],
+  })
+);
+
+app.use(express.json());
+
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mti5t.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+
+    // Database Collections
+    const parcelCollection = client.db("parcelDB").collection("parcels");
+    const userCollection = client.db("parcelDB").collection("users");
+
+
+    // Users APIs
+    app.post("/users", async (req, res) => {
+      const newUser = req.body;
+      const bodyEmail = { email: newUser.email };
+      const existingEmail = await userCollection.findOne(bodyEmail);
+      if (existingEmail) {
+        return;
+      }
+      const result = await userCollection.insertOne(newUser);
+      res.send(result);
+    });
+
+    // Get User by email
+    app.get("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const filter = { email: email };
+      const result = await userCollection.findOne(filter);
+      res.send(result);
+    });
+
+
+
+
+
+    // All "Delivery Men"
+    app.get("/allDeliveryMan", async (req, res) => {
+      const filter = { userRole: "Delivery Men" };
+      const result = await userCollection.find(filter).toArray();
+      res.send(result);
+    });
+
+    // Book A Parcel Post
+    app.post("/parcel-book", async (req, res) => {
+      const data = req.body;
+      const result = await parcelCollection.insertOne(data);
+      res.send(result);
+    });
+
+    // Get Booked Parcel for user
+    app.get("/my-parcel-book/:email", async (req, res) => {
+      const email = req.params.email;
+      const filter = { email: email };
+      const result = await parcelCollection.find(filter).toArray();
+      res.send(result);
+    });
+
+    // Get One parcel by id
+    app.get("/my-parcel-book/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const result = await parcelCollection.findOne(filter);
+      res.send(result);
+    });
+
+    // Update parcel by id
+    app.put("/my-parcel-book/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedDoc = req.body;
+      const newData = {
+        $set: {
+          phoneNumber: updatedDoc.phoneNumber,
+          parcelWeight: updatedDoc.parcelWeight,
+          parcelType: updatedDoc.parcelType,
+          receiverPhoneNumber: updatedDoc.receiverPhoneNumber,
+          receiverName: updatedDoc.receiverName,
+          parcelDeliveryAddress: updatedDoc.parcelDeliveryAddress,
+          requestedDeliveryDate: updatedDoc.requestedDeliveryDate,
+          price: updatedDoc.price,
+          latitude: updatedDoc.latitude,
+          longitude: updatedDoc.longitude,
+          status: updatedDoc.status,
+          date: updatedDoc.date,
+        }
+      }
+      const result = parcelCollection.updateOne(filter, newData, options)
+      res.send(result)
+    })
+
+    // Cancel Parcel Order by id
+    app.patch("/my-parcel-book/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedDoc = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const newData = {
+        $set: updatedDoc 
+      }
+      const result = await parcelCollection.updateOne(filter, newData);
+      res.send(result)
+    })
+
+
+    //Admin Statistics
+    app.get("/admin-statistics", async (req, res) => {
+      const parcels = await parcelCollection.estimatedDocumentCount();
+      const users = await userCollection.estimatedDocumentCount();
+
+      res.send({
+        parcels,
+        users,
+      });
+    });
+
+    // Statistics Parcel Booked By date
+    app.get("/booked-by-date", async (req, res) => {
+      const parcels = await parcelCollection
+        .aggregate([
+          {
+            $addFields: {
+              formattedDate: {
+                $dateToString: {
+                  format: "%d-%m-%Y",
+                  date: { $toDate: "$date" },
+                },
+              },
+            },
+          },
+          {
+            $group: {
+              _id: "$formattedDate",
+              count: { $sum: 1 },
+            },
+          },
+          { $sort: { _id: 1 } },
+        ])
+        .toArray();
+      res.send(parcels);
+    });
+
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Assignment 12 Starting");
+});
+
+app.listen(port, () => {
+  console.log(`Assignment-12 is sitting on port ${port}`);
+});
